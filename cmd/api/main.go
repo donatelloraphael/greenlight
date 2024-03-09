db3db9e1cbc63629e4ac11b49b7941fd86387c38
Akbar E K <akbar72987@gmail.com>
2024-03-09 11:46:22 +0530

    add api rate limiting

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -28,6 +29,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 // application struct holds the dependencies for our HTTP handlers, helpers, and middleware.
@@ -38,21 +44,72 @@ type application struct {
 }
 
 func main() {
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+		logger.PrintFatal(err, nil)
 	}
 
 	var cfg config
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.url, "POSTGRESQL_URL", os.Getenv("POSTGRESQL_URL"), "PostgreSQL DSN")
-	flag.IntVar(&cfg.db.maxOpenConns, "POSTGRESQL_MAX_OPEN_CONNS", 25, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleConns, "POSTGRESQL_MAX_IDLE_CONNS", 25, "PostgreSQL max idle connections")
-	flag.StringVar(&cfg.db.maxIdleTime, "POSTGRESQL_MAX_IDLE_TIME", "15m", "PostgreSQL max connection idle time")
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		logger.PrintFatal(fmt.Errorf("invalid port %s", err), nil)
+	}
+	flag.IntVar(&cfg.port, "PORT", port, "API server port")
+
+	environment := os.Getenv("ENVIRONEMNT")
+	if _, ok := map[string]bool{"development": true, "staging": true, "production": true}[environment]; !ok {
+		logger.PrintFatal(fmt.Errorf("invalid environment %s", environment), nil)
+	}
+	flag.StringVar(&cfg.env, "ENVIRONEMNT", "development", "Environment (development|staging|production)")
+
+	postgresUrl := os.Getenv("POSTGRESQL_URL")
+	if postgresUrl == "" {
+		logger.PrintFatal(fmt.Errorf("POSTGRESQL_URL is not set"), nil)
+	}
+	flag.StringVar(&cfg.db.url, "POSTGRESQL_URL", postgresUrl, "PostgreSQL DSN")
+
+	postgresMaxOpenConns, err := strconv.Atoi(os.Getenv("POSTGRESQL_MAX_OPEN_CONNS"))
+	if err != nil {
+		logger.PrintFatal(fmt.Errorf("invalid POSTGRESQL_MAX_OPEN_CONNS %s", err), nil)
+	}
+	flag.IntVar(&cfg.db.maxOpenConns, "POSTGRESQL_MAX_OPEN_CONNS",  postgresMaxOpenConns, "PostgreSQL max open connections")
+
+	postgresMaxIdleConns, err := strconv.Atoi(os.Getenv("POSTGRESQL_MAX_IDLE_CONNS"))
+	if err != nil {
+		logger.PrintFatal(fmt.Errorf("invalid POSTGRESQL_MAX_IDLE_CONNS %s", err), nil)
+	}
+	flag.IntVar(&cfg.db.maxIdleConns, "POSTGRESQL_MAX_IDLE_CONNS", postgresMaxIdleConns, "PostgreSQL max idle connections")
+
+	postgresMaxIdleTime := os.Getenv("POSTGRESQL_MAX_IDLE_TIME")
+	if postgresMaxIdleTime == "" {
+		logger.PrintFatal(fmt.Errorf("invalid POSTGRESQL_MAX_IDLE_TIME %s", err), nil)
+	}
+	flag.StringVar(&cfg.db.maxIdleTime, "POSTGRESQL_MAX_IDLE_TIME", postgresMaxIdleTime, "PostgreSQL max connection idle time")
+
+	limiterRps, err := strconv.ParseFloat(os.Getenv("LIMITER_RPS"), 64)
+	if err != nil {
+		logger.PrintFatal(fmt.Errorf("invalid LIMITER_RPS %s", err), nil)
+	} 
+	flag.Float64Var(&cfg.limiter.rps, "LIMITER_RPS", limiterRps, "Rate limiter maximum requests per second")
+
+	limiterBurst, err := strconv.Atoi(os.Getenv("LIMITER_BURST"))
+	if err != nil {
+		logger.PrintFatal(fmt.Errorf("invalid LIMITER_BURST %s", err), nil)
+	}
+	flag.IntVar(&cfg.limiter.burst, "LIMITER_BURST", limiterBurst, "Rate limiter maximum burst")
+
+	limiterEnabled, err := strconv.ParseBool(os.Getenv("LIMITER_ENABLED"))
+	if err != nil {
+		logger.PrintFatal(fmt.Errorf("invalid LIMITER_ENABLED %s", err), nil)
+	}
+	flag.BoolVar(&cfg.limiter.enabled, "LIMITER_ENABLED", limiterEnabled, "Enable rate limiter")
+
 	flag.Parse()
 
-	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+	fmt.Println(cfg)
 
 	db, err := openDB(cfg)
 	if err != nil {
